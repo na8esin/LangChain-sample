e745c11e02463becd9b6a2f6e7c76c359b70744e
sikmi_watanabe <na8esin.mpst@gmail.com>
2024-04-26 22:44:46 +0900

    google_search_bedrock: コマンドライン引数追加

--- google_search_bedrock.py
+++ google_search_bedrock.py
@@ -1,6 +1,7 @@
 # https://qiita.com/cyberBOSE/items/a331ed830ed1859a198d
 # https://python.langchain.com/docs/modules/agents/agent_types/xml_agent/
 
+import sys
 from langchain import hub
 from langchain_aws import ChatBedrock
 from langchain.agents import create_xml_agent
@@ -16,6 +17,7 @@ load_dotenv()
 # GOOGLE_API_KEYは無料のfirebaseプロジェクトでも取得可能
 search = GoogleSearchAPIWrapper()
 
+# プロンプトのテンプレート
 prompt = hub.pull("hwchase17/xml-agent-convo")
 
 # 使用可能なツールと説明
@@ -36,5 +38,5 @@ agent = create_xml_agent(llm, tools, prompt)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True)
 
 # Agentの実行
-answer = agent_executor.invoke({"input": "本日の日本のニュースはどんなものがありますか？日本語でお答えください。"})
+answer = agent_executor.invoke({"input": sys.argv[1]})
 print(answer['output'])
